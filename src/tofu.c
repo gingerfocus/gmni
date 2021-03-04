10aace0d2083a452222a31dfaddfbf15984d8d41
Drew DeVault <sir@cmpwn.com>
2021-03-04 12:58:50 -0500

    Discard CA certs unless there's no other

--- src/tofu.c
+++ src/tofu.c
@@ -63,6 +63,9 @@ xt_end_cert(const br_x509_class **ctx)
 		cc->err = err;
 		return;
 	}
+	if (br_x509_decoder_isCA(&cc->decoder) && cc->pkey) {
+		return;
+	}
 	cc->pkey = br_x509_decoder_get_pkey(&cc->decoder);
 	br_sha512_out(&cc->sha512, &cc->hash);
 }
